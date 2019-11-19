87f062db0be141daa27e370aa9e2ee096c433f65
NaoArimura <naoyosetani@gmail.com>
2019-11-19 21:03:17 +0900

    succeed to get data

--- getEvents.js
+++ getEvents.js
@@ -1,33 +1,32 @@
-const axios = require("axios");
+const unirest = require("unirest");
+var convert = require("xml-js");
 
-var unirest = require("unirest");
+const getEvents = (keywords, location, category, date) => {
+  const req = unirest(
+    "GET",
+    "https://community-eventful.p.rapidapi.com/events/search"
+  );
 
-var req = unirest(
-  "GET",
-  "https://community-eventful.p.rapidapi.com/events/search"
-);
+  req.query({
+    keywords: keywords,
+    location: location,
+    category: category,
+    date: date,
+    app_key: "Kt6kcJL4XfPs4wp5"
+  });
 
-req.query({
-  keywords: "book",
-  location: "Tokyo",
-  category: "",
-  date: "",
-  app_key: "Kt6kcJL4XfPs4wp5"
-});
+  req.headers({
+    "x-rapidapi-host": "community-eventful.p.rapidapi.com",
+    "x-rapidapi-key": "9c43912371msh14d997739e2be0fp16cc17jsn6def561079f0"
+  });
 
-unirest
-  .post("https://community-eventful.p.rapidapi.com/events/search")
-  .headers({ Accept: "application/json", "Content-Type": "application/json" })
-  .send(req.query);
+  req.end(function(res) {
+    if (res.error) throw new Error(res.error);
 
-// req.headers({
-//   "x-rapidapi-host": "community-eventful.p.rapidapi.com",
-//   "x-rapidapi-key": "9c43912371msh14d997739e2be0fp16cc17jsn6def561079f0"
-// });
+    const result = convert.xml2js(res.body, { compact: true, spaces: 4 });
+    console.log(result.search.events.event[0].title);
+  });
+};
 
-req.end(function(res) {
-  if (res.error) throw new Error(res.error);
-
-  console.log(res);
-  // console.log(JSON.parse(res.body));
-});
+//TEST
+getEvents("book", "Tokyo");
