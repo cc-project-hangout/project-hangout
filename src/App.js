7d8602b390b86ead74d776fe1f8b015bad2a56f1
Igor Michailov <igor.m.byak@gmail.com>
2019-11-19 22:56:55 +0900

    done the basic structure for SEARCH in APP.jsx and some css

--- src/App.js
+++ src/App.js
@@ -17,25 +17,50 @@ export default class App extends React.Component {
     };
   }
 
+  handleSeachClicked = (e) => {
+    e.preventDefault();
+    //add input validation
+
+    console.log("started search");
+    this.setState({filtered: true})
+  }
+
+  handleBackOrCancel = (e) => {
+    e.preventDefault();
+
+    this.setState({filtered: false});
+  }
+
   render() {
     return (
       <div className="App">
-        <form className="user-input">
-          <input type="text" placeholder="City"></input>
-          <input type="date" placeholder="Arrival Date"></input>
-          <input type="date" placeholder="Departure Date"></input>
-          <div>
-            <input type="number" className="hotel-price" pattern="[0-9]*" min="0" max="9999" placeholder="Hotel Min Price"></input>
-            <input type="number" className="hotel-price" pattern="[0-9]*" min="0" max="9999" placeholder="Hotel Max Price"></input>
-          </div>
-          <input type="number" pattern="[0-9]*" min="0" max="9999" placeholder="Play Budget"></input>
-          <p>
-            THIS IS A PLACEHOLDER FOR ALL THE INFO
-          </p>
-          <p>
-            HELLO THERE
-          </p>
-        </form>
+        { !this.state.filtered ? (
+          <form className="user-input" value="">
+            <input type="text" placeholder="City"></input>
+            <label for="startDate">arrival Date</label>
+            <input id="startDate" type="date"></input>
+            <input type="date" placeholder="Departure Date"></input>
+            <div className="two-column-div">
+              <input type="number" className="hotel-price" pattern="[0-9]*" min="0" max="9999" placeholder="Hotel Min Price"></input>
+              <input type="number" className="hotel-price" pattern="[0-9]*" min="0" max="9999" placeholder="Hotel Max Price"></input>
+            </div>
+            <input type="number" pattern="[0-9]*" min="0" max="9999" placeholder="Play Budget"></input>
+
+            <div className="two-column-div">
+              <button className="form-button" onClick={(e) => {this.handleSeachClicked(e)}}> SEARCH </button>
+              <button className="form-button" onClick={(e) => {this.handleBackOrCancel(e)}}> CLEAR </button>
+            </div>
+          </form>
+        ) : (
+          <form className="user-input" value="">
+            <button className="goHome" onClick={(e) => {this.handleBackOrCancel(e)}}> GO BACK TO SEARCH </button>
+            <p>
+              THIS IS A PLACEHOLDER FOR ALL THE INFO
+            </p>
+
+          </form>
+
+        ) }
       </div>
     );
   }
