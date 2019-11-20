41b56a57f0da4e8cfa16b17914cdc9afd11a4b96
NaoArimura <naoyosetani@gmail.com>
2019-11-20 10:14:27 +0900

    made function which picks info from eventful api

--- getEvents.js
+++ getEvents.js
@@ -1,16 +1,16 @@
 const unirest = require("unirest");
 var convert = require("xml-js");
 
-const getEvents = (keywords, location, category, date) => {
+const getEvents = (city, date) => {
   const req = unirest(
     "GET",
     "https://community-eventful.p.rapidapi.com/events/search"
   );
 
   req.query({
-    keywords: keywords,
-    location: location,
-    category: category,
+    // keywords: keywords,
+    location: city,
+    // category: category,
     date: date,
     app_key: "Kt6kcJL4XfPs4wp5"
   });
@@ -23,10 +23,16 @@ const getEvents = (keywords, location, category, date) => {
   req.end(function(res) {
     if (res.error) throw new Error(res.error);
 
+    //do it map
     const result = convert.xml2js(res.body, { compact: true, spaces: 4 });
-    console.log(result.search.events.event[0].title);
+    console.log(`title`, result.search.events.event[0].title._text);
+    console.log(`start time`, result.search.events.event[0].start_time._text);
+    console.log(`venue name`, result.search.events.event[0].venue_name._text);
+    console.log(`venue url`, result.search.events.event[0].venue_url._text);
+    console.log(`description`, result.search.events.event[0].description._text);
+    console.log(`image url`, result.search.events.event[0].image.url._text);
   });
 };
 
 //TEST
-getEvents("book", "Tokyo");
+getEvents("Tokyo", "2019112000");
