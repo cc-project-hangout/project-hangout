9c8ec25cfb2da8ee8e7409012198357aca377a56
Igor Michailov <igor.m.byak@gmail.com>
2019-11-20 20:42:18 +0900

    added logos, moved assets

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import Events from "./components/Events";
 import Hotels from "./components/Hotels";
+import logo_name from "./assets/logo_name_white.png";
+
 export default class App extends React.Component {
   constructor() {
     super();
@@ -14,12 +16,12 @@ export default class App extends React.Component {
         departDate: "",
         minPrice: 0,
         maxPrice: 9999,
-        budget: 0
+        budget: 0,
       },
       datepicker: {
         arriveDate: "text",
         departDate: "text",
-      }
+      },
     };
   }
 
@@ -29,15 +31,15 @@ export default class App extends React.Component {
       console.log("Params are OK.Started search.");
       this.setState({ filtered: true });
     } else {
-      alert("Check Your Input");
+      this.setState({ dataInvalid: true });
     }
-
   };
 
   handleBackOrCancel = e => {
     e.preventDefault();
 
     this.setState({
+      dataInvalid: false,
       filtered: false,
       selections: {
         cityName: "",
@@ -45,7 +47,7 @@ export default class App extends React.Component {
         departDate: "",
         minPrice: 0,
         maxPrice: 9999,
-        budget: 0
+        budget: 0,
       },
     });
   };
@@ -56,46 +58,72 @@ export default class App extends React.Component {
       newType = "date";
     }
     if (e.target.id === "arriveDate") {
-      this.setState({ datepicker: {arriveDate: newType}})
+      this.setState({ datepicker: { arriveDate: newType } });
     } else if (e.target.id === "departDate") {
-      this.setState({ datepicker: {departDate: newType}})
+      this.setState({ datepicker: { departDate: newType } });
     }
   }
 
-  setCity = (e) => {
-    this.setState({ selections: {...this.state.selections, cityName: e.target.value}});
-  }
-  setArrival = (e) => {
+  setCity = e => {
+    this.setState({ selections: { ...this.state.selections, cityName: e.target.value } });
+  };
+  setArrival = e => {
     const arrivalDateUnix = new Date(e.target.value).getTime();
-    const oneDay = 24*60*60*1000;
+    const oneDay = 24 * 60 * 60 * 1000;
     const nextDayUnix = arrivalDateUnix + oneDay;
-    const nextDay = new Date(nextDayUnix)
-    const nextDayString = `${nextDay.getUTCFullYear()}-${String(nextDay.getUTCMonth() + 1).padStart(2, "0")}-${String(nextDay.getUTCDate()).padStart(2, "0")}`
-    this.setState({ selections: {...this.state.selections, arriveDate: e.target.value, departDate: nextDayString}});
-  }
-  setDeparture = (e) => {
-    this.setState({ selections: {...this.state.selections, departDate: e.target.value}});
-  }
-  setHotelMin = (e) => {
-    this.setState({ selections: {...this.state.selections, minPrice: e.target.value}});
-  }
-  setHotelMax = (e) => {
-    this.setState({ selections: {...this.state.selections, maxPrice: e.target.value}});
-  }
-  setBudget = (e) => {
-    this.setState({ selections: {...this.state.selections, budget: e.target.value}});
-  }
-  
-  formValidation= () => {
+    const nextDay = new Date(nextDayUnix);
+    const nextDayString = `${nextDay.getUTCFullYear()}-${String(nextDay.getUTCMonth() + 1).padStart(2, "0")}-${String(
+      nextDay.getUTCDate()
+    ).padStart(2, "0")}`;
+    this.setState({ selections: { ...this.state.selections, arriveDate: e.target.value, departDate: nextDayString } });
+  };
+  setDeparture = e => {
+    this.setState({ selections: { ...this.state.selections, departDate: e.target.value } });
+  };
+  setHotelMin = e => {
+    this.setState({ selections: { ...this.state.selections, minPrice: e.target.value } });
+  };
+  setHotelMax = e => {
+    this.setState({ selections: { ...this.state.selections, maxPrice: e.target.value } });
+  };
+  setBudget = e => {
+    this.setState({ selections: { ...this.state.selections, budget: e.target.value } });
+  };
+
+  formValidation = () => {
     const city = this.state.selections.cityName;
     const startDateUnix = new Date(this.state.selections.arriveDate);
     const endDateUnix = new Date(this.state.selections.departDate);
-    return city !== "" && this.state.selections.arriveDate !== "" && this.state.selections.departDate !== "" && endDateUnix > startDateUnix;
-  }
+    return (
+      city !== "" &&
+      this.state.selections.arriveDate !== "" &&
+      this.state.selections.departDate !== "" &&
+      endDateUnix > startDateUnix
+    );
+  };
 
   render() {
     return (
       <div className="App">
+        {this.state.dataInvalid ? (
+          <div
+            id="blinder"
+            onClick={() => {
+              this.setState({ dataInvalid: false });
+            }}
+          >
+            <h1> Please Check you input! </h1>
+            <h2> Please check that the City Name is entered </h2>
+            <h2> Please select a planned arrival date </h2>
+            <h2> Please make sure that the planned departure date is after the arrival date </h2>
+          </div>
+        ) : (
+          <div />
+        )}
+        <div className="app-name">
+          <img className="app-name" src={logo_name} alt="the logo of EvenTrip" />
+        </div>
+        {/* <h1 className="app-name">Eventrip</h1> */}
         {!this.state.filtered ? (
           <form className="user-input" value="">
             <input id="cityName" type="text" placeholder="City" onChange={this.setCity}></input>
@@ -103,8 +131,12 @@ export default class App extends React.Component {
               <input
                 id="arriveDate"
                 type={this.state.datepicker.arriveDate}
-                onFocus={(e) => {this.handleDateField(e)}}
-                onBlur={(e) => {this.handleDateField(e)}}
+                onFocus={e => {
+                  this.handleDateField(e);
+                }}
+                onBlur={e => {
+                  this.handleDateField(e);
+                }}
                 onChange={this.setArrival}
                 className="date-pick"
                 placeholder="Arrival Date"
@@ -112,8 +144,12 @@ export default class App extends React.Component {
               <input
                 id="departDate"
                 type={this.state.datepicker.departDate}
-                onFocus={(e) => {this.handleDateField(e)}}
-                onBlur={(e) => {this.handleDateField(e)}}
+                onFocus={e => {
+                  this.handleDateField(e);
+                }}
+                onBlur={e => {
+                  this.handleDateField(e);
+                }}
                 onChange={this.setDeparture}
                 // min={th}
                 value={this.state.selections.departDate}
@@ -141,7 +177,14 @@ export default class App extends React.Component {
                 onChange={this.setHotelMax}
               ></input>
             </div>
-            <input type="number" pattern="[0-9]*" min="0" max="9999" placeholder="Play Budget $" onChange={this.setBudget}></input>
+            <input
+              type="number"
+              pattern="[0-9]*"
+              min="0"
+              max="9999"
+              placeholder="Play Budget $"
+              onChange={this.setBudget}
+            ></input>
 
             <div className="two-column-div">
               <button
@@ -166,18 +209,18 @@ export default class App extends React.Component {
           </form>
         ) : (
           <div id="componentContainer">
-          <form className="goHomeForm" value="">
-            <button
-              className="goHome"
-              onClick={e => {
-                this.handleBackOrCancel(e);
-              }}
-            >
-              {" "}
-              GO BACK TO SEARCH{" "}
-            </button>
-          </form>
-          <div>
+            <form className="goHomeForm" value="">
+              <button
+                className="goHome"
+                onClick={e => {
+                  this.handleBackOrCancel(e);
+                }}
+              >
+                {" "}
+                GO BACK TO SEARCH{" "}
+              </button>
+            </form>
+            <div>
               <Events
                 cityName={this.state.selections.cityName}
                 arriveDate={this.state.selections.arriveDate}
@@ -198,4 +241,3 @@ export default class App extends React.Component {
     );
   }
 }
-
