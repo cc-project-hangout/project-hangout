9f0dc26ec2b29d80bebf5f0a19431464507f9f56
NaoArimura <naoyosetani@gmail.com>
2019-11-21 20:16:19 +0900

    succeeded to get multiple data from ticketmaster

--- server/index.js
+++ server/index.js
@@ -4,9 +4,6 @@ const app = express();
 const { loadHotels } = require("./utils/Hotels/loadHotels");
 const { getEvents } = require("./utils/Events/getEvents");
 
-// const morgan = require("morgan");
-// app.use(morgan("dev"));
-
 app.use("/api", express.json(), express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "../build/")));
@@ -24,7 +21,6 @@ app.post("/api/hotels", async (req, res) => {
 app.post("/api/events", async (req, res) => {
   try {
     const events = await getEvents(req.body.city, req.body.date);
-    // console.log(`events in server`, events);
     res.json(events);
   } catch (e) {
     throw new Error("event error");
--- server/utils/Events/getEvents.js
+++ server/utils/Events/getEvents.js
@@ -1,8 +1,6 @@
-// const unirest = require("unirest");
 const axios = require("axios");
 var convert = require("xml-js");
 require("dotenv").config();
-
 const getEvents = (city, date) => {
   const eventsFromEventful = eventsFromEventfulApi(city, date);
   const eventsFromTcketmaster = eventsFromTicketmasterApi(city, date);
@@ -11,9 +9,7 @@ const getEvents = (city, date) => {
     return allEvents;
   });
 };
-
 const eventsFromEventfulApi = (city, date) => {
-  console.log("start eventful");
   return axios
     .get(
       `https://community-eventful.p.rapidapi.com/events/search?location=${city}&date=${date}&app_key=${process.env.EVENTFUL_API_APP_KEY}`,
@@ -30,7 +26,6 @@ const eventsFromEventfulApi = (city, date) => {
         compact: true,
         spaces: 2,
       }).search.events.event;
-
       arrayOfEventArray.forEach(eventfulObj => {
         const sortedObj = {
           title: eventfulObj["title"]["_text"],
@@ -47,57 +42,37 @@ const eventsFromEventfulApi = (city, date) => {
         }
         arrayOfInfoWeNeed.push(sortedObj);
       });
-      console.log("finish eventful");
-      // console.log(`array we need`, arrayOfInfoWeNeed);
       return arrayOfInfoWeNeed;
     });
 };
-
 const eventsFromTicketmasterApi = (city, date) => {
   const formatDate = date => {
-    return date.substring(0, 4) + "-" + date.substring(4, 6) + "-" + date.substring(6, 8);
+    return date.substring(0, 4) + "-" + date.substring(4, 6) + "-" + date.substring(6, 8) + "T09:00:00Z";
   };
   const formattedData = formatDate(date);
-  console.log(`formattedData`, formattedData);
-
-  // const countryCode = "US";
-
-  // `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=${countryCode}&apikey=iaQO4AwkJX6SRwqiuoeSMBoOANkvVap3`
-  // `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.TICKETMASTER_API_KEY}`
-  // `https://app.ticketmaster.com/discovery/v2/events.json?startDateTime=${formattedData}&apikey=${process.env.TICKETMASTER_API_KEY}`
-  // `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=${countryCode}&localStartDateTime=${formattedData}&apikey=${process.env.TICKETMASTER_API_KEY}`
-  // `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&&apikey=${process.env.TICKETMASTER_API_KEY}`
-
   return axios
     .get(
-      // `https://app.ticketmaster.com/discovery/v2/events.json?$starDateTime="2019-12-04T14:00:00Z"&&apikey=${process.env.TICKETMASTER_API_KEY}`
-      `https://app.ticketmaster.com/discovery/v2/events.json?$endDateTime="2019-12-04T14:00:00Z"&&apikey=${process.env.TICKETMASTER_API_KEY}`
-      // `https://app.ticketmaster.com/discovery/v2/events.json?$startDateTime=${formattedData}&&apikey=${process.env.TICKETMASTER_API_KEY}`
-      // `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&&startDateTime=${formattedData}&&apikey=${process.env.TICKETMASTER_API_KEY}`
+      `https://app.ticketmaster.com/discovery/v2/events.json?$startDateTime=city=${city}&&apikey=${process.env.TICKETMASTER_API_KEY}`
     )
     .then(res => {
       const arrayOfInfoWeNeed = [];
-      console.log("the second api is working");
       const data = res.data._embedded.events[0];
-      // console.log(res.data._embedded.events);
-      console.log(`data`, data);
-      const sortedObj = {
-        title: data["name"],
-        image: data["images"][0]["url"],
-        venue: data["_embedded"]["venues"][0]["name"],
-        url: data["url"],
-        description: "More detail on the link",
-        startTime: data["dates"]["start"]["localDate"],
-      };
-      // console.log(sortedObj);
-      arrayOfInfoWeNeed.push(sortedObj);
-      // console.log(arrayOfInfoWeNeed);
-      console.log("finish second function");
-      // console.log("array from ticketmaster", arrayOfInfoWeNeed);
+      const arrayOfEventArray = res.data._embedded.events;
+      arrayOfEventArray.forEach(ticketMasterObj => {
+        const sortedObj = {
+          title: ticketMasterObj["name"],
+          image: ticketMasterObj["images"][0]["url"],
+          venue: ticketMasterObj["_embedded"]["venues"][0]["name"],
+          url: ticketMasterObj["url"],
+          description: "More detail on the link",
+          startTime: ticketMasterObj["dates"]["start"]["localDate"],
+        };
+        arrayOfInfoWeNeed.push(sortedObj);
+      });
+
       return arrayOfInfoWeNeed;
     });
 };
-
 module.exports = {
   getEvents,
 };
